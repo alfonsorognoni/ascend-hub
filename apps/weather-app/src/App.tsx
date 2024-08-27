3b65969f8eebc80f324caed0771804ed9d27f76c
Alfonso Rognoni <alfonsorognoni@gmail.com>
2024-08-27 19:01:26 -0300

    feat: adding weather app

--- apps/weather-app/src/App.tsx
+++ apps/weather-app/src/App.tsx
@@ -1,40 +1,43 @@
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import "./App.css";
-
-const apiKey = import.meta.env.VITE_WEATHER_API_KEY;
-const apiUrl = import.meta.env.VITE_WEATHER_API_URL;
-
-// get location from autocomplete
-const getLocation = async ({ search }) => {
-  const response = await fetch(
-    `${apiUrl}/search.json?key=${apiKey}&q=${search}`
-  );
-  const [data] = await response.json();
-  return data;
-};
-
-// get weather data for location
-const getWeather = async ({ location }) => {
-  const response = await fetch(
-    `${apiUrl}/current.json?key=${apiKey}&q=${location}`
-  );
-  const data = await response.json();
-  return data;
-};
+import { getLocation, Location } from "./libs/getLocation";
+import { useDebounce } from "./hooks/useDebounce";
+import { getWeather, Weather } from "./libs/getWeather";
 
 function App() {
-  const [location, setLocation] = useState("");
-  const [weather, setWeather] = useState(null);
+  const inputRef = useRef<HTMLInputElement>(null);
+  const [search, setSearch] = useState("");
+  const [locationList, setLocationList] = useState<Location[]>([]);
+  const [location, setLocation] = useState<string | null>(null);
+  const [weather, setWeather] = useState<Weather | null>(null);
+
+  const debouncedSearch = useDebounce(search, 500);
+
+  const handleSearch = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setSearch(e.target.value);
+  };
+
+  const handleLocation = (location: string) => {
+    setLocation(location);
+    setLocationList([]);
+    if (inputRef.current) {
+      inputRef.current.value = "";
+    }
+  };
 
   useEffect(() => {
-    getLocation({ search: "New York" }).then((location) => {
-      setLocation(location);
+    if (!debouncedSearch || debouncedSearch.length < 3) {
+      setLocationList([]);
+      return;
+    }
+    getLocation({ search: debouncedSearch }).then(({ data }) => {
+      setLocationList(data);
     });
-  }, []);
+  }, [debouncedSearch]);
 
   useEffect(() => {
     if (!location) return;
-    getWeather({ location: location.name }).then((weather) => {
+    getWeather({ location }).then((weather) => {
       setWeather(weather);
       console.log(weather);
     });
@@ -61,10 +64,27 @@ function App() {
               className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
               placeholder="name@flowbite.com"
               required
+              onChange={handleSearch}
+              ref={inputRef}
             />
+
+            {locationList.length > 0 && (
+              <div className="flex flex-col gap-2">
+                {locationList.map((location) => (
+                  <div
+                    onClick={() => handleLocation(location.name)}
+                    key={location.id}
+                    className="flex items-center gap-2 px-4 py-2 text-sm text-gray-900 rounded-lg cursor-pointer bg-gray-50 dark:bg-gray-700 dark:text-white"
+                  >
+                    <p className="font-medium">{location.name}</p>
+                    <p className="text-gray-500 truncate">{location.region}</p>
+                  </div>
+                ))}
+              </div>
+            )}
           </div>
           <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-            {location.name}
+            {location}
           </h5>
           <p className="font-normal text-gray-700 dark:text-gray-400">
             {weather?.current?.temp_c} °C
---
+++ apps/weather-app/src/libs/getLocation.ts
@@ -0,0 +1,29 @@
+// get location from autocomplete
+const apiKey = import.meta.env.VITE_WEATHER_API_KEY;
+const apiUrl = import.meta.env.VITE_WEATHER_API_URL;
+
+export interface Location {
+  id: number;
+  name: string;
+  region: string;
+  country: string;
+  lat: number;
+  lon: number;
+  url: string;
+}
+
+interface LocationResponse {
+  data: Location[];
+}
+
+export const getLocation = async ({
+  search,
+}: {
+  search: string;
+}): Promise<LocationResponse> => {
+  const response = await fetch(
+    `${apiUrl}/search.json?key=${apiKey}&q=${search}`
+  );
+  const data = await response.json();
+  return { data };
+};
